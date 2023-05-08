c808d2d4c82c6165782362e109cdcfb66fb7dea5
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-08 15:18:49 +0530

    subnet_groups

--- main.tf
+++ main.tf
@@ -5,3 +5,34 @@ resource "aws_db_subnet_group" "subnet_group" {
   tags = merge (local.common_tags, { Name = "${var.env}-docdb_subnet_group" } )
 
 }
+
+resource "aws_security_group" "docdb" {
+  name        = "${var.env}-docdb_security_group"
+  description = "${var.env}-docdb_subnet_group"
+  vpc_id = var.vpc_id
+
+
+  ingress {
+    description      = "MongoDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge (local.common_tags, { Name = "${var.env}-docdb_subnet_group" } )
+
+}
+
+output "sg_out_id" {
+  value = aws_security_group.allow_tls.id
+}
