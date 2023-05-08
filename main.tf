e88948a1ac918a6fcbb035a51df616b51c140f73
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-08 19:20:06 +0530

    subnet_groups

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "aws_docdb_cluster" "docdb" {
   engine                                  = "docdb"
   master_username                         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password                         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
+  skip_final_snapshot                     = true
   db_subnet_group_name                    = aws_db_subnet_group.subnet_group.name
   vpc_security_group_ids                  = [aws_security_group.docdb.id]
   tags = merge (local.common_tags, { Name = "${var.env}-docdb_subnet_group" } )
